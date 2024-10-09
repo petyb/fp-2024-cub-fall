8824c45bf62a66270d7c42b9ecfee3230b4278df
petyb <Pborovlev@gmail.com>
2024-10-09 15:26:28 +0200

    removed redundant comments, fixed all warnings

--- expr/src/Expr.hs
+++ expr/src/Expr.hs
@@ -22,34 +22,3 @@ instance Show Expr where
   show (Div expr1 expr2) = "(" ++ show expr1 ++ ") / (" ++ show expr2 ++ ")"
   show (Pow expr1 expr2) = "(" ++ show expr1 ++ ") ^ (" ++ show expr2 ++ ")"
   show (Let var expr1 expr2) = "Let " ++ var ++ " = " ++ show expr1 ++ " in " ++ show expr2
-
--- eval :: M.Map String Expr -> Expr -> Maybe Int
--- eval _ (Lit n) = Just n
--- eval state (Plus x y) =
---   case (eval state x, eval state y) of
---     (Just x, Just y) -> Just $ x + y
---     _ -> Nothing
--- eval state (Var v) = do
---   case M.lookup v state of
---     Just v -> eval state v
---     Nothing -> Nothing
-
--- run :: Expr -> M.Map String Expr -> IO ()
--- run expr state = do
---   print expr
---   print state
---   print (eval state expr)
---   putStrLn ""
-
--- main = do
---   let expr1 = Var "x"
---   let expr2 = Plus (Lit 2) (Lit 2)
---   let expr3 = Plus (Var "x") (Lit 1)
---   let state1 = M.fromList [("x", Lit 42), ("y", Lit 13)]
---   let state2 = M.empty
---   run expr1 state1
---   run expr2 state1
---   run expr3 state1
---   run expr1 state2
---   run expr2 state2
---   run expr3 state2
--- expr/test/Spec.hs
+++ expr/test/Spec.hs
@@ -1,14 +1,12 @@
 import qualified Data.Map.Strict as M
-import Data.Maybe (isNothing)
 import Interpreter
 import qualified Expr
 import qualified Data.Map as Map
 import qualified Error
 import Test.Tasty (TestTree, defaultMain, testGroup)
-import Test.Tasty.HUnit (assertBool, assertFailure, testCase, (@?=))
-import qualified Error
+import Test.Tasty.HUnit (testCase, (@?=))
 
--- testBasicOperations :: TestTree
+testBasicOperations :: TestTree
 testBasicOperations =
     testGroup "Basic Operations" [
       testCase "2 == 2" $ eval M.empty (Expr.Num 2) @?= Right 2,
@@ -20,19 +18,21 @@ testBasicOperations =
       testCase "2 ^ 3 == 8" $ eval M.empty (Expr.Pow (Expr.Num 2) (Expr.Num 3)) @?= Right 8
     ]
 
+testComplexExpressions :: TestTree
 testComplexExpressions =
     testGroup "Complex Expressions" [
       testCase "(2 + 3) * 2 == 10" $ eval M.empty (Expr.Mul (Expr.Add (Expr.Num 2) (Expr.Num 3)) (Expr.Num 2)) @?= Right 10,
       testCase "sqrt(2 ^ 4) - 1 == 3" $ eval M.empty (Expr.Sub (Expr.Sqrt (Expr.Pow (Expr.Num 2) (Expr.Num 4))) (Expr.Num 1)) @?= Right 3
     ]
 
-
+testErrorsWithoutVariables :: TestTree
 testErrorsWithoutVariables =
     testGroup "Errors Without Variables" [
       testCase "2 / 0 - can't divide by zero" $ eval M.empty (Expr.Div (Expr.Num 2) (Expr.Num 0)) @?= Left (Error.ZeroDivision (Expr.Div (Expr.Num 2) (Expr.Num 0) )),
       testCase "sqrt(-1) - can't take square root of a negative number" $ eval M.empty (Expr.Sqrt (Expr.Num (-1))) @?= Left (Error.NegativeNumberSqrt (Expr.Sqrt (Expr.Num (-1))))
     ]
 
+testBasicOperationsWithVariables :: TestTree
 testBasicOperationsWithVariables = 
   testGroup "Basic Operations With Variables" [
       testCase "v = 2, if v = 2" $ eval (Map.singleton "v" 2) (Expr.Var "v") @?= Right 2,
@@ -43,18 +43,21 @@ testBasicOperationsWithVariables =
       testCase "v ^ 3 = 8, if v = 2" $ eval (Map.singleton "v" 2) (Expr.Pow (Expr.Var "v") (Expr.Num 3)) @?= Right 8
     ]
 
+testComplexExpressionsWIthVariables :: TestTree
 testComplexExpressionsWIthVariables =
   testGroup "Complex Expressions With Variables" [
       testCase "(a + 2) * b = 10, if a = 3, b = 2" $ eval (Map.fromList [("a", 3), ("b", 2)]) (Expr.Mul (Expr.Add (Expr.Var "a") (Expr.Num 2)) (Expr.Var "b")) @?= Right 10,
       testCase "(a / b) * c = 1, if a = 3, b = 2, c = 0" $ eval (Map.fromList [("a", 3), ("b", 2), ("c", 0)]) (Expr.Pow (Expr.Div (Expr.Var "a") (Expr.Var "b")) (Expr.Var "c")) @?= Right 1
     ] 
 
+testComplexExpressionsWithLet :: TestTree
 testComplexExpressionsWithLet = 
   testGroup "Complex Expressions With Let" [
     testCase "Let x = a + 1 in (x * 2) ^ b should be 9 if a = 0.5, b = 2" $ eval (Map.fromList [("a", 0.5), ("b", 2)]) (Expr.Let "x" (Expr.Add (Expr.Var "a") (Expr.Num 1)) (Expr.Pow (Expr.Mul (Expr.Var "x") (Expr.Num 2)) (Expr.Var "b"))) @?= Right 9,
     testCase "Let x = 1 in Let y = (x + 1) in y * 2 should be 4" $ eval Map.empty (Expr.Let "x" (Expr.Num 1) (Expr.Let "y" (Expr.Add (Expr.Var "x") (Expr.Num 1)) (Expr.Mul (Expr.Var "y") (Expr.Num 2)))) @?= Right 4
   ]
 
+testErrorsWithVariablesAndLet :: TestTree
 testErrorsWithVariablesAndLet =
   testGroup "Errors With Variables And Let" [
     testCase "2 + a - where a is undefined" $ eval (Map.singleton "b" 2) (Expr.Add (Expr.Num 2) (Expr.Var "a")) @?= Left (Error.UndefinedVar "a"),
