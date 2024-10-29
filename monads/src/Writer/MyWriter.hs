0f5255bb6aefbe1d44e3e998e005e8e23f34a3a1
Ekaterina Verbitskaia <kajigor@gmail.com>
2024-10-30 00:48:22 +0100

    MyWriter

--- monads/src/Writer/MyWriter.hs
+++ monads/src/Writer/MyWriter.hs
@@ -14,15 +14,23 @@ censor f (MyWriter (w, x)) = undefined
 
 instance Functor (MyWriter w) where 
   fmap :: (a -> b) -> MyWriter w a -> MyWriter w b 
-  fmap f (MyWriter (w, a)) = undefined
-    
+  fmap f (MyWriter (w, a)) = MyWriter (w, f a) 
+  -- fmap (f . g) == fmap f . fmap g 
+  -- \forall (MyWriter (w, a)): 
+  --   fmap (f . g) (MyWriter (w, a)) == (fmap f . fmap g) (MyWriter (w, a)) 
+  --   ==  MyWriter (w, (f . g) a) ==  MyWriter (w, f (g a)) == fmap f (MyWriter (w, g a)) == 
+  --       fmap f (fmap g (MyWriter (w, a))) == (fmap f . fmap g) (MyWriter (w, a))
+
+
 instance Monoid w => Applicative (MyWriter w) where 
   pure :: a -> MyWriter w a
-  pure x = undefined
+  pure x = MyWriter (mempty, x)
 
   (<*>) :: MyWriter w (a -> b) -> MyWriter w a -> MyWriter w b
-  MyWriter (w, f) <*> MyWriter (w', x) = undefined
+  MyWriter (w, f) <*> MyWriter (w', x) = MyWriter (w <> w', f x)
 
 instance Monoid w => Monad (MyWriter w) where 
   (>>=) :: MyWriter w a -> (a -> MyWriter w b) -> MyWriter w b 
-  MyWriter (w, x) >>= f = undefined
+  MyWriter (w, x) >>= f = 
+    let MyWriter (w', b) = f x in 
+    MyWriter (w <> w', b)
